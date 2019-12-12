ee0abd36a191c2dbec29785432e202e2340b6c01
Kher <etienne.kher@laplateforme.io>
2019-12-12 01:10:30 +0100

    protec insc

--- inscription.php
+++ inscription.php
@@ -5,21 +5,28 @@
 		header("Location: index.php");
 	}
 	if(isset($_POST['inscr']))
-	{	if($_POST['mdp']==$_POST['remdp'])
-		{	$req=sql("SELECT * FROM `utilisateurs` WHERE `login`='".$_POST['login']."';");
-			if(empty($req))
-			{
-				sql("INSERT INTO `utilisateurs`(`id`, `login`, `password`) VALUES (null,'".$_POST['login']."','".chiffre($_POST['mdp'])."');");
-				header("Location: connexion.php");
-			}
-			else
-			{
-				$err="<p id='err'>Login deja pris, essayé ".$_POST['login']."123</p>";
-			}
+	{	if (strlen($_POST['login'])==0 || strlen($_POST['mdp'])==0) 
+		{
+			$err="<p id='err'>bien essayé hackerman</p>";
 		}
 		else
 		{
-			$err="<p id='err'>Les mots de passe saisis sont différent</p>";
+			if($_POST['mdp']==$_POST['remdp'])
+			{	$req=sql("SELECT * FROM `utilisateurs` WHERE `login`='".$_POST['login']."';");
+				if(empty($req))
+				{
+					sql("INSERT INTO `utilisateurs`(`id`, `login`, `password`) VALUES (null,'".$_POST['login']."','".chiffre($_POST['mdp'])."');");
+					header("Location: connexion.php");
+				}
+				else
+				{
+					$err="<p id='err'>Login deja pris, essayé ".$_POST['login']."123</p>";
+				}
+			}
+			else
+			{
+				$err="<p id='err'>Les mots de passe saisis sont différent</p>";
+			}
 		}
 	}
 ?>
@@ -32,9 +39,9 @@
 <body>
 	<?php include('header.php') ?>
 		<form action="Inscription.php" method="post">
-			<label>Login :</label><input type="text" name="login">
-			<label></label><input type="password" name="mdp">
-			<label></label><input type="password" name="remdp">
+			<label>Login :</label><input type="text" name="login" required>
+			<label></label><input type="password" name="mdp" required>
+			<label></label><input type="password" name="remdp" required>
 			<input type="submit" name="inscr">
 		</form>
 	<?php
